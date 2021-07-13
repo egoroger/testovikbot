9e0dbf94d46f555f1bc18f7c2f815b538d8b0ab1
egoroger <87011692+egoroger@users.noreply.github.com>
2021-07-13 14:11:48 +0500

    Add files via upload

---
+++ testovik/ClassUserStor.cs
@@ -0,0 +1,24 @@
+﻿namespace testovik
+{
+    public class TgUser
+    {
+        public string name;
+        public bool istester = false;
+        public TgUser(string _name, bool _istester) {name=_name; istester=_istester;}
+    } //пользователь
+    public class UserStor
+    {
+        public TgUser[] users;
+        public int size, count;
+        public UserStor(int size) {this.size=size;users=new TgUser[size];count=0;}
+        ~UserStor() {}
+        public void addu(TgUser user) {users[count]=user; count++;}
+        public void deluser(int index)
+        {
+            if (count==1) users[count-1] = null;
+            else if (count>1) for (int i=index; i<count; i++) users[i]=users[i+1];
+            count--;
+        }
+        public TgUser getu(int index) {return users[index];}
+    } //список пользователей
+}
